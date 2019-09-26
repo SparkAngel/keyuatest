eb95b99df51c1b998ee82f8127e6ff60c110949f
SparkAngel <igor.gnatenko555@gmail.com>
2019-09-26 12:32:59 +0300

    Updates

---
+++ precache-manifest.c05320cffe03c616632524c392817330.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef06f340f2ae68eb6b4a5d1072f63f3",
+    "url": "/keyuatest/index.html"
+  },
+  {
+    "revision": "c770ca99063e10faf3c7",
+    "url": "/keyuatest/static/css/2.bf6ebad0.chunk.css"
+  },
+  {
+    "revision": "639f50a4fb488930ced7",
+    "url": "/keyuatest/static/css/main.9b433a36.chunk.css"
+  },
+  {
+    "revision": "c770ca99063e10faf3c7",
+    "url": "/keyuatest/static/js/2.ab879ae4.chunk.js"
+  },
+  {
+    "revision": "639f50a4fb488930ced7",
+    "url": "/keyuatest/static/js/main.5f96f07e.chunk.js"
+  },
+  {
+    "revision": "a2b913d57aeb631d97a6",
+    "url": "/keyuatest/static/js/runtime~main.d21fd8da.js"
+  },
+  {
+    "revision": "b066d79b8e5e56229a0e37f9327de265",
+    "url": "/keyuatest/static/media/logoAnnual.b066d79b.svg"
+  },
+  {
+    "revision": "cb62980fed59cd48088b9c9e01458d3b",
+    "url": "/keyuatest/static/media/logoTrial.cb62980f.svg"
+  },
+  {
+    "revision": "f51ab00bc16b8e56136ce832db5c2fbf",
+    "url": "/keyuatest/static/media/logoYes.f51ab00b.svg"
+  },
+  {
+    "revision": "baa3aa3dff2e55d111e403dd8c77013f",
+    "url": "/keyuatest/static/media/logocompany.baa3aa3d.svg"
+  },
+  {
+    "revision": "5d4361b90fc9dfc7b3d8b4686346c7e1",
+    "url": "/keyuatest/static/media/logopayments.5d4361b9.svg"
+  },
+  {
+    "revision": "4fd3c1c0d9165f7e4d2d30a2b08630d3",
+    "url": "/keyuatest/static/media/man.4fd3c1c0.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/keyuatest/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/keyuatest/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/keyuatest/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/keyuatest/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "73da265fc7be761510b9583825d7ba47",
+    "url": "/keyuatest/static/media/woman.73da265f.png"
+  }
+]);
